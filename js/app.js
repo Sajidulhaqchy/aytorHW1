86b35107e84e0bd4e8525b117404b9defb0b6539
sajidul haque chowdhury <151939538+Sajidulhaqchy@users.noreply.github.com>
2024-05-05 17:21:48 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+
+    // *SLICKSLIDER(banner)
+    $('.bannerSlider').slick({
+      arrows:false,
+      dots:true,
+      dotsClass:'bannerDots container',
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  });
